b5c19224219cda828e0493ca251df6932177d255
TrilemmaGrande <93474242+TrilemmaGrande@users.noreply.github.com>
2022-12-21 14:24:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var teamsArr = [];
+
+
+function addTeam(){
+    let teamsInput = "";
+    var temp = "";
+    const maxTeams = 4;
+    document.getElementById("inputteambtn").focus();
+    document.getElementById("inputteam").select();
+
+    teamsInput = document.getElementById("inputteam").value;
+    if (teamsArr.includes(teamsInput)) {
+        alert("name already given!")
+    }
+    else if (teamsInput==""){
+        alert("name can't be nothing")
+    }   
+    else {   
+        teamsArr.push(teamsInput);        
+        temp = teamsArr.length;
+        document.getElementById("team"+temp).toggleAttribute("hidden");
+        document.getElementById("points"+temp).toggleAttribute("hidden");
+        document.getElementById("team"+temp).innerText=teamsInput;        
+    }
+    if (teamsArr.length >= maxTeams){
+        document.getElementById("inputteam").setAttribute("hidden","null");
+        document.getElementById("inputteambtn").setAttribute("hidden","null");
+        document.getElementById("inputteamdonebtn").setAttribute("hidden","null");
+    }
+}
+
+function addTeamDone(){
+    document.getElementById("inputteam").setAttribute("hidden","null");
+    document.getElementById("inputteambtn").setAttribute("hidden","null");
+    document.getElementById("inputteamdonebtn").setAttribute("hidden","null");
+}
+
+function removeTeam(count){
+    null;
+}
