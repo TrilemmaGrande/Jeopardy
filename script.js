bf584a53b4a230dd5ff6784ee9cfcde7ade96324
TrilemmaGrande <psychokater@protonmail.com>
2022-12-22 12:16:15 +0100

    implemented questions, still fucked.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var teamsArr = [];
 var flag = 0;
+var choosenQuestions = [];
 
 function addTeam(){
     let teamsInput = "";
@@ -10,34 +11,41 @@ function addTeam(){
 
     teamsInput = document.getElementById("inputteam").value;
     if (teamsArr.includes(teamsInput)) {
-        alert("name already given!")
+        alert("name already given!");
     }
     else if (teamsInput==""){
-        alert("name can't be nothing")
+        alert("name can't be nothing");
     }   
     else {   
         teamsArr.push(teamsInput);        
         temp = teamsArr.length;
         document.getElementById("team"+temp).toggleAttribute("hidden");
         document.getElementById("points"+temp).toggleAttribute("hidden");
-        document.getElementById("team"+temp).innerText=teamsInput;        
+        document.getElementById("team"+temp).innerText=teamsInput;             
     }
     if (teamsArr.length >= maxTeams){
         document.getElementById("inputteam").setAttribute("hidden","null");
         document.getElementById("inputteambtn").setAttribute("hidden","null");
-        flag = 1
+        flag = 1;
     }
     if (temp == 1 && flag == 0){
         document.getElementById("removeteambtn").removeAttribute("hidden");
-        flag = 1
+        document.getElementById("inputteamdonebtn").toggleAttribute("hidden");
+        flag = 1;
     }
 }
 
 function addTeamDone(){
     document.getElementById("inputteam").setAttribute("hidden","null");
     document.getElementById("inputteambtn").setAttribute("hidden","null");
-    document.getElementById("inputteamdonebtn").setAttribute("hidden","null");
-    document.getElementById("removeteambtn").setAttribute("hidden","null");
+    document.getElementById("inputteamdonebtn").toggleAttribute("hidden");
+    document.getElementById("removeteambtn").toggleAttribute("hidden");
+
+    for (let i = 1; i < teamsArr.length; i++){
+        document.getElementById("teambtn"+i).toggleAttribute("hidden");
+        document.getElementById("teambtnright"+i).toggleAttribute("hidden");
+        document.getElementById("teambtnwrong"+i).toggleAttribute("hidden");
+    }
 }
 
 function removeTeam(){
@@ -45,10 +53,28 @@ function removeTeam(){
     temp = teamsArr.length
     document.getElementById("team"+temp).toggleAttribute("hidden");
     document.getElementById("points"+temp).toggleAttribute("hidden");
+    document.getElementById("inputteam").removeAttribute("hidden");
+    document.getElementById("inputteambtn").removeAttribute("hidden");
+    
     teamsArr.pop();
     temp = teamsArr.length
     if (temp == 0 && flag == 1){
         document.getElementById("removeteambtn").setAttribute("hidden","null");
+        document.getElementById("inputteamdonebtn").toggleAttribute("hidden");
         flag = 0
     }
+}
+
+function openQuestion(choosenField){
+    document.getElementById("field"+choosenField).style.backgroundColor = "#878a87";
+    if (choosenQuestions.includes(choosenField) == false){
+        document.getElementById("overlay").toggleAttribute("hidden");
+        document.getElementById("q"+choosenField).toggleAttribute("hidden");
+        choosenQuestions.push(choosenField);
+    }  
+}
+
+function toggleQuestionHidden(){
+    document.getElementById("overlay").toggleAttribute("hidden");
+    document.getElementById("q"+choosenQuestions[choosenQuestions.length -1]).toggleAttribute("hidden");
 }
